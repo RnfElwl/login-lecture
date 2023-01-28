166f69afadd695c29bb6d23bba6890c64f2e5062
백승엽 <cherryhey2004@gmail.com>
2023-01-29 01:08:53 +0900

    DOM을 이용하여 HTML 객체에 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login (){
+    const req = {
+        id : id.value,
+        psword : password.value,
+    };
+}
